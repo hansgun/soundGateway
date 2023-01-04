436c7d5b2e475eb1b6eea8ca9c0e2819330dbcbe
hansgun <gmos@nate.com>
2023-01-04 18:44:42 +0900

    # runner

--- sound/snd_loader.py
+++ sound/snd_loader.py
@@ -9,6 +9,10 @@ Created on Mon Mar 15 17:54:34 2021
 import os
 import soundfile as sf
 
+HOME_PATH = os.path.join(os.path.dirname(os.path.abspath(__file__)), os.pardir)
+MODULE_DIR = 'util'
+sys.path.append(os.path.join(HOME_PATH, MODULE_DIR))
+
 class snd_loader:
     def __init__(self, file_path, len_snd, interval_snd):
         self.file_path = file_path
@@ -36,12 +40,10 @@ if __name__ == "__main__":
     #SND_FILE_PATH = '/Users/han/Documents/code/python/sound/from_source/'
     FILE_NAME = 'real.wav'
     print(os.path.join(SND_FILE_PATH,FILE_NAME))
-    samplerate, result_list = snd_loader(os.path.join(SND_FILE_PATH,FILE_NAME), 3, 2).get_snd_df()
+    samplerate, result_list = snd_loader(os.path.join(SND_FILE_PATH,FILE_NAME), 1, 1).get_snd_df()
     
     print(list(result_list[0]))
     
-    import sys
-    sys.path.append('/Users/han/Documents/code/python/sound/farms')
-    import snd_dns_cal    
+    import snd_dns_cal  
     test = snd_dns_cal.snd_dns_cal().set_snd_data(result_list[3],samplerate).cal_dns_mat()
     print(test)
--- sound/snd_runner.py
+++ sound/snd_runner.py
@@ -1,7 +1,12 @@
-import os
+import os, pickle
 from glob import glob
+import snd_loader
+import snd_dns_cal_new
+import matplotlib.pyplot as plt 
+import numpy as np 
+import pandas as pd
 
-data_dir = './data/'
+data_dir = '../data/ILT/'
 
 sub_dir = [x[0] for x in sorted(os.walk(data_dir))][1:]
 wav_names = []
@@ -9,7 +14,51 @@ wav_names = []
 for sub_d in sub_dir :
     wav_names.extend(glob(sub_d+'/'+'*.wav'))
 
-list_param = [('/'.join(x.split('/')[:-1]), os.path.splitext(x.split('/')[-1])[0]) for x in wav_names]
+list_param = [('/'.join(x.split('/')[:-1]) + '/', x.split('/')[-1]) for x in wav_names]
 
-for x1, x2 in list_param :
-    os.system(f"python3.9 snd_dns_cal_new.py {x1} {x2}")
+
+df = pd.DataFrame([[None, None]], columns = ['fileName', 'score'])
+
+
+for i, (x1, x2) in enumerate(list_param) :
+    wav_file_str = os.path.join(x1 + '/' + x2)
+    if i % 100 == 0 : print(f"i : {i}/{len(list_param)}")
+    # if i > 2 : break
+    samplerate, sliced = snd_loader.snd_loader(wav_file_str, 1, 1).get_snd_df()
+
+    # 4. reshape
+    reshaped = snd_dns_cal_new.reshape_snd_data(np.array(sliced))
+    # 5. list divide by neighbor_size & search_size. to pages
+    # paged_norm_sliced = get_pages(reshaped)
+
+    # 6. calculate dns matrix
+    result = snd_dns_cal_new.cal_dns_mat(np.asarray(reshaped))
+
+    # 7. normalization
+    result = snd_dns_cal_new.normalization(result)
+    #print('elapsed : ', time.time() - start_time)
+
+    # 7. save result to pickle data format
+    # import pickle
+    #
+    # with open(os.path.join(HOME_PATH,OUTPUT_DIR,'data_real_wave_680.pickle'), 'wb') as f:
+    #    pickle.dump(result, f, pickle.HIGHEST_PROTOCOL)
+
+    # 8. ploting
+    
+    # 8. create directory 
+    snd_dns_cal_new.createFolder('../output/ilt_20211108_20211109/')
+    #print(f'result shape : {result.shape}')
+    for i in range(result.shape[0]) : 
+        # fig_name =  sys.argv[1] + '/result_image/' + sys.argv[2] + '_' + str(i) +'.png'
+        fig_name =  '../output/ilt_20211108_20211109/' + x2.split('.')[0] + '_' + str(i).zfill(2) +'.jpg'
+
+        plt.imsave(fig_name, result[i,:,:], cmap='gray')
+
+        score_temp = str(np.round(np.mean(result[:,:,i]),2))
+        df.loc[len(df)] = [x2.split('.')[0] + '_' + str(i).zfill(2) +'.jpg',score_temp]
+        # print(fig_name + '/' + score_temp)
+
+
+with open('../output/ilt_1108_1109.pickle','wb') as f : 
+    pickle.dump(df,f)
